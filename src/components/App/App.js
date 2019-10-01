b112445baec0f3b2f3710b248c2c9bcec01a23e0
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-01 12:32:46 -0600

    Add functionality to switch news card category

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import technology from '../../data/technology'
+import entertainment from '../../data/entertainment'
+import science from '../../data/science'
+import health from '../../data/health'
 import './App.css';
+import Menu from '../Menu/Menu.js';
 import NewsContainer from '../NewsContainer/NewsContainer.js';
+
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      dataRepo: {
+        local: local,
+        technology: technology,
+        entertainment: entertainment,
+        science: science,
+        health: health
+      },
       data: local
     }
+  }
+  updateData = (dataSet) => {
+    this.setState({ data: dataSet })
+  }
+
+  handleClick(event) {
+    this.updateData(event.target.dataset.set);
+    console.log(event.target.dataset.set);
   }
 
   render() {
     return (
       <div className="div div--app">
+        <Menu dataSets={this.state.dataRepo} method={this.updateData}/>
         <NewsContainer data={this.state.data} />
       </div>
     );
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
-  <ul className='ul ul--menu'>
-    <li>Local News</li>
-    <li>Technology</li>
-    <li>Entertainment</li>
-    <li>Science</li>
-    <li>Health</li>    
+
+
+
+const Menu = (props) => {
+  return <ul className='ul ul--menu'>
+    <li data-set='local' className='li li--local' onClick={event => props.method(props.dataSets.local)}>Local News</li>
+    <li data-set='technology' className='li li--tech' onClick={event => props.method(props.dataSets.technology)}>Technology</li>
+    <li data-set='entertainment' className='li li--entertainment' onClick={event => props.method(props.dataSets.entertainment)}>Entertainment</li>
+    <li data-set='science' className='li li--science' onClick={event => props.method(props.dataSets.science)}>Science</li>
+    <li data-set='health' className='li li--health' onClick={event => props.method(props.dataSets.health)}>Health</li>    
   </ul>
 }
 
