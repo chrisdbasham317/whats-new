1ab92e54875fdd72bd5bbcd5d01f593c447485bf
Chris Basham <chrisdbasham0317@gmail.com>
2019-09-30 19:49:38 -0600

    Add temporary styles to the news container/cards

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,8 +3,8 @@ import './NewsArticle.css';
 
 const NewsArticle = ({ id, headline, img, description, url }) => {
   console.log()
-  return <section>
-    <img data={id} src={img} />
+  return <section className='section section--news-cards'>
+    <img alt='' className='img img--card-pic' data={id} src={img} />
     <h2>{headline}</h2>
     <article>{description}</article>
     <footer data={url}>Link to Article</footer>
