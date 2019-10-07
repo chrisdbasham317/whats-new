2f1368cdcdf0b32d12a436ed52b1aac26158838f
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-07 00:15:57 -0600

    Add fetch to app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import local from '../../data/local';
-import technology from '../../data/technology';
-import entertainment from '../../data/entertainment'
-import science from '../../data/science'
-import health from '../../data/health'
 import './App.css';
 import Menu from '../Menu/Menu.js';
 import NewsContainer from '../NewsContainer/NewsContainer.js';
@@ -16,17 +11,27 @@ class App extends Component {
     super();
     this.state = {
       categories: ['local', 'technology', 'entertainment', 'science', 'health'],
-      dataRepo: {
-        local: local,
-        technology: technology,
-        entertainment: entertainment,
-        science: science,
-        health: health
-      },
-      currentCategory: local,
-      data: local
+      dataRepo: {},
+      currentCategory: {},
+      data: {},
+      loading: true
     }
   }
+
+  componentWillMount() {
+    fetch("https://whats-new-api.herokuapp.com/api/v1/news")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        return this.setState({
+          loading: false,
+          dataRepo: data,
+          currentCategory: data.local,
+          data: data.local,
+        });
+      });
+  }
+
   updateData = dataSet => {
     this.setState({
       currentCategory: dataSet,
@@ -46,22 +51,27 @@ class App extends Component {
   }
 
   render() {
-    const menuButtons = this.state.categories.map(category => <Menu key={category} text={category} dataSets={this.state.dataRepo} updateData={this.updateData}/>)
+    const { categories, dataRepo, currentCategory, data, loading } = this.state;
+    const menuButtons = this.state.categories.map(category => <Menu key={category} text={category} dataSets={this.state.dataRepo} updateData={this.updateData} />);
     return (
-      <div className='div div--app'>
-        <aside className='aside aside--menu'>
-          <ul className='ul ul--menu'>
-          {menuButtons}
-          </ul>
-        </aside>
-        <nav className='nav nav--search'>
-          <h1 className='h1 h1--app-title'>What's <span className='span span--title'>New?</span></h1>
-          <SearchForm searchArticles={this.searchArticles}/>
-        </nav>
-        <main className='main main--news-container'>
-          <NewsContainer data={this.state.data} />
-        </main>  
-      </div>
+      <div>
+        {!loading ? (
+          <div className='div  div--app'>
+            <aside className='aside aside--menu'>
+              <ul className='ul ul--menu'>
+                {menuButtons}
+              </ul>
+            </aside>
+            <nav className='nav nav--search'>
+              <h1 className='h1 h1--app-title'>What's <span className='span span--title'>New?</span></h1>
+              <SearchForm searchArticles={this.searchArticles} />
+            </nav>
+            <main className='main main--news-container'>
+              <NewsContainer data={this.state.data} />
+          </main>
+        </div>) : <div>'Loading'</div>
+        }
+      </div> 
     );
   }
 }
